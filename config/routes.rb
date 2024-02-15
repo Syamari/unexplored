a305d9dda636540610f29f6cdf2623ffc16fe236
Syamari <130308232+Syamari@users.noreply.github.com>
2024-02-16 06:31:44 +0900

    feat: sorceryによるユーザー登録機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
 end
