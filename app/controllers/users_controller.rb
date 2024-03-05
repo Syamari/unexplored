2581cc1fc11ee69e01a861f79258ffc2b15d4428
Syamari <130308232+Syamari@users.noreply.github.com>
2024-03-06 00:30:32 +0900

    feat: ユーザー情報編集ページを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'ユーザー情報を更新しました'
+      redirect_to @user
+    else
+      flash.now[:error] = 'ユーザー情報の更新に失敗しました'
+      render :edit
+    end
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create]
+  resources :users, only: %i[new create edit update]
 
 
   resources :lists do
