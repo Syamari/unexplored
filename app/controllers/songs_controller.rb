a61d997f1149d261b66a2dda4022939dee490523
Syamari <130308232+Syamari@users.noreply.github.com>
2024-03-07 00:57:00 +0900

    feat: レコメンド楽曲画面へ戻るボタンなどを設置し、他の画面同様にレイアウトを整理

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,8 +7,18 @@ class SongsController < ApplicationController
   #before_action :check_api_limit
 
   def show
+    if @list.artists.count < 3
+      flash[:info] = 'レコメンドを行うにはアーティストを3人以上追加してください'
+      redirect_to @list
+      return
+    end
+
+    #ダミーロジック用のコードです
+    @recommend_genre = "math rock"
+
     @unique_genres = get_unique_genre_names(@list)
-    @recommend_genre = get_recommend_genre(@unique_genres)
+    #一時的にコメントアウトして代わりにダミーのコードを使えます
+    #@recommend_genre = get_recommend_genre(@unique_genres)
     searched_playlists = RSpotify::Playlist.search(@recommend_genre)
 
     if searched_playlists.empty?
@@ -17,8 +27,8 @@ class SongsController < ApplicationController
 			return
 		end
     
-    selected_song = searched_playlists.first(4).max_by { |playlist| playlist.followers['total'] }.tracks.sample
-    url = selected_song.embed.match(/https:\/\/embed\.spotify\.com\/\?uri=spotify:track:(\w+)/)
+    @selected_song = searched_playlists.first(3).max_by { |playlist| playlist.followers['total'] }.tracks.sample
+    url = @selected_song.embed.match(/https:\/\/embed\.spotify\.com\/\?uri=spotify:track:(\w+)/)
     @player_url = "https://open.spotify.com/embed/track/#{url[1]}"
     session[:visited] = true
   end
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -8,3 +8,13 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+document.getElementById('RecommendButton').addEventListener('click', function() {
+    this.outerHTML = `
+      <div class="flex justify-center" aria-label="Loading">
+        <div class="animate-ping h-2 w-2 bg-blue-500 rounded-full"></div>
+        <div class="animate-ping h-2 w-2 bg-purple-500 rounded-full mx-4"></div>
+        <div class="animate-ping h-2 w-2 bg-rose-500 rounded-full"></div>
+      </div>
+    `;
+  });
