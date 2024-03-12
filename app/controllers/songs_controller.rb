6316e18324d0f2f0215a3b69649ebb3609f6941a
Syamari <130308232+Syamari@users.noreply.github.com>
2024-03-13 00:14:38 +0900

    fix: ジャンル名出力のロジックを変更

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,7 +18,7 @@ class SongsController < ApplicationController
 
     @unique_genres = get_unique_genre_names(@list)
     #一時的にコメントアウトして代わりにダミーのコードを使えます
-    #@recommend_genre = get_recommend_genre(@unique_genres)
+    @recommend_genre = get_recommend_genre(@unique_genres)
     searched_playlists = RSpotify::Playlist.search(@recommend_genre)
 
     if searched_playlists.empty?
@@ -30,6 +30,7 @@ class SongsController < ApplicationController
     @selected_song = searched_playlists.first(3).max_by { |playlist| playlist.followers['total'] }.tracks.sample
     url = @selected_song.embed.match(/https:\/\/embed\.spotify\.com\/\?uri=spotify:track:(\w+)/)
     @player_url = "https://open.spotify.com/embed/track/#{url[1]}"
+    generate_genre_description
     session[:visited] = true
   end
 
@@ -42,6 +43,19 @@ class SongsController < ApplicationController
   def redirect_if_reloaded
     if session[:visited]
       redirect_to lists_path
+    end
+  end
+
+  def generate_genre_description
+    genre1 = @selected_song.artists.first&.genres&.first&.titleize
+    genre2 = @recommend_genre.titleize
+  
+    if genre1.blank?
+      @genre_description = genre2
+    elsif genre1 == genre2
+      @genre_description = genre1
+    else
+      @genre_description = "#{genre1} / #{genre2}"
     end
   end
 
