055e9274d65cb8eb59f2635ab1d64b733a3ca69f
Syamari <130308232+Syamari@users.noreply.github.com>
2024-03-15 22:58:00 +0900

    fix: OGP設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,31 @@ module ApplicationHelper
 		unique_genre_names
 	end
 
+	def default_meta_tags
+    {
+      site: 'Unexplored Music',
+      title: '新ジャンル開拓のための音楽レコメンドサービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Unexplored Musicは未知の音楽ジャンルのレコメンドを体験できるサービスです。',
+      keywords: '音楽,ジャンル,楽曲',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('card2.jpg'), # 配置するパスやファイル名によって変更すること
+        local: 'ja-JP'
+      },
+      # Twitter用の設定を個別で設定する
+      twitter: {
+        card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
+        site: '@SyamariTech', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
+        image: image_url('card2.jpg') # 配置するパスやファイル名によって変更すること
+      }
+    }
+  end
 end
