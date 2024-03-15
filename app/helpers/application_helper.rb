73a4f656cad16e33ba4fcfa4b48966cb18ea1b28
Syamari <130308232+Syamari@users.noreply.github.com>
2024-03-15 23:20:50 +0900

    fix: OGPの画像修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,14 +23,14 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('card2.jpg'), # 配置するパスやファイル名によって変更すること
+        image: image_url('card3.jpg'), # 配置するパスやファイル名によって変更すること
         local: 'ja-JP'
       },
       # Twitter用の設定を個別で設定する
       twitter: {
         card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
         site: '@SyamariTech', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
-        image: image_url('card2.jpg') # 配置するパスやファイル名によって変更すること
+        image: image_url('card3.jpg') # 配置するパスやファイル名によって変更すること
       }
     }
   end
