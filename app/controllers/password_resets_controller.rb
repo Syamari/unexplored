8c1bba620ef94f2f937ae28d94fa39317908ee01
Syamari <130308232+Syamari@users.noreply.github.com>
2024-04-14 00:45:51 +0900

    Lint修正 & 環境変更

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,19 +1,17 @@
 class PasswordResetsController < ApplicationController
-
-
   def new; end
-
-  def create
-    @user = User.find_by(email: params[:email])
-    @user&.deliver_reset_password_instructions!
-    redirect_to login_path
-    flash[:success]= 'パスワードリセットのメールを送信しました'
-  end
 
   def edit
     @token = params[:id]
     @user = User.load_from_reset_password_token(params[:id])
     not_authenticated if @user.blank?
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    @user&.deliver_reset_password_instructions!
+    redirect_to login_path
+    flash[:success] = 'パスワードリセットのメールを送信しました'
   end
 
   def update
@@ -30,13 +28,13 @@ class PasswordResetsController < ApplicationController
       @user.update(reset_password_token: nil)
       logger.debug "Password has been successfully updated."
       redirect_to login_path
-      flash[:success]= 'パスワードがリセットされました'
+      flash[:success] = 'パスワードがリセットされました'
     else
       respond_to do |format|
         format.turbo_stream do
           flash.now[:error] = "パスワードのリセットに失敗しました。パスワードは英小文字と数字を含む8文字以上が必要です。"
           render turbo_stream: turbo_stream.replace("flash_message", partial: "shared/flash_message")
-        end   
+        end
       end
       logger.debug "Failed to update password."
     end
