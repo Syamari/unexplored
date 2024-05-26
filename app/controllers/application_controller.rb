9f047c099398a3150b3b0073252c54c0f5560a38
Syamari <130308232+Syamari@users.noreply.github.com>
2024-05-27 00:37:09 +0900

    fix: プロンプトの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,15 +34,31 @@ class ApplicationController < ActionController::Base
         messages: [
           {
             role: 'user',
-            content: "You are a pop music genre recommendation professional familiar with the 15 major musics of Pop, R&B, Soul, Rock, Alternative, Indie, Country, Electronic, Folk, Hiphop, Jazz, Metal, Punk, Blues, Experimental.
-            Now, I provide you with information on the genres that a given user likes and the artists they are most likely to like. So please perform the following task considering the granularity of the music genres, BPM, and dynamics tendencies contained in those pieces of information as professional.
-            Task: Guess, with rigorous deliberation, 10 popular music genres that can be expressed in 1-3 words and that have little relevance to the genres contained in this array, i.e., that this user HAS NOT YET listened to. Then, output these 10 genres in the Specified Format. These 10 genres must be strictly selected from the designated genres described below. If there are instructions for the Designated Country, be sure to adhere strictly to follow it. Do not write any text. Be sure to output only the Specified Format data to me. The name of the genre must be written in English.
-						This is a complex task where multiple conditions may overlap, so always be with caution and strict adherence to the status of all conditions.
-            ##this User's favorite music genres: #{genres}
-            ##Artists this user is likely to like: #{names}
-            ##Designated Genre : #{genre}
-            ##Designated Country: #{language}
-            ##Specified format: genre1, genre2, genre3, ... , genre1"
+            content: "
+						You are a popular music genre recommendation professional.
+						I provide you with information on the genres that a given user likes and the artists they are most likely to like. 
+						Please perform the following task considering the granularity of the music genres, BPM, and dynamic tendencies contained in that information as a professional.
+						
+						## Task:
+						Guess, with rigorous deliberation, 10 popular music genres that meet the following criteria:
+						1. The genres must be strictly selected from the designated genres described below.
+						2. The genres must have little relevance to the genres contained in the provided array, i.e., the user has not yet listened to them.
+						3. Each genre must be expressed in 1-3 words.
+						4. The name of the genre must be written in English.
+						5. Adhere strictly to the Designated Country if specified.
+						6. Adhere strictly to the Designated Genre if specified.
+						Output these 10 genres in the Specified Format.
+						
+						## Input:
+						- User's favorite music genres: #{genres}
+						- Artists this user is likely to like: #{names}
+						- Designated Genre: #{genre}
+						- Designated Country: #{language}
+						
+						## Specified Format:
+						genre1, genre2, genre3, ..., genre10
+						**Output only the Specified Format data. Do not write any explanatory text.**
+						This is a complex task where multiple conditions may overlap, so always be cautious and strictly adhere to the status of all conditions."
           }
         ]
       }
