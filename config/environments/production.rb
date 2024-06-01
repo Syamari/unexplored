e33e3edc7e7757aac390a2aea056e7964f8e83c2
Syamari <130308232+Syamari@users.noreply.github.com>
2024-06-02 03:07:38 +0900

    fix: google認証の環境別コールバックURLを設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  
 end
