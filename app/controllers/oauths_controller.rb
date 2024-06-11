97eb81f3c3c8441961a5189e7f6770c25177b7f0
Syamari <130308232+Syamari@users.noreply.github.com>
2024-06-12 07:50:17 +0900

    fix: Googleログイン後のリダイレクト先の修正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,12 +8,12 @@ class OauthsController < ApplicationController
     provider = auth_params[:provider]
     # 既存のユーザーをプロバイダ情報を元に検索し、存在すればログイン
     if (@user = login_from(provider))
-      redirect_to root_path, notice: "#{provider.titleize}アカウントでログインしました"
+      redirect_to lists_path, notice: "#{provider.titleize}アカウントでログインしました"
     else
       begin
         # ユーザーが存在しない場合はプロバイダ情報を元に新規ユーザーを作成し、ログイン
         signup_and_login(provider)
-        redirect_to root_path, notice: "#{provider.titleize}アカウントでログインしました"
+        redirect_to lists_path, notice: "#{provider.titleize}アカウントでログインしました"
       rescue StandardError => e
         Rails.logger.error "Googleログインエラー: #{e.message}"
         redirect_to root_path, alert: "#{provider.titleize}アカウントでのログインに失敗しました"
