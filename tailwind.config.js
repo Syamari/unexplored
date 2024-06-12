013f7464841d19c615d80b77a7f4106bbd461775
Syamari <130308232+Syamari@users.noreply.github.com>
2024-06-13 00:12:37 +0900

    add: tailwind.config.jsへのanimationの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,11 @@ module.exports = {
             "swirl-in-fwd": "swirl-in-fwd 0.6s ease   both",
             "roll-out-right": "roll-out-right 12.0s ease   both",
             "rotate-out-center": "rotate-out-center 0.5s cubic-bezier(0.550, 0.085, 0.680, 0.530)   both",
-            "scale-in-hor-center": "scale-in-hor-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both"
+            "scale-in-hor-center": "scale-in-hor-center 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both",
+            "fade-out-top": "fade-out-top 0.7s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both",
+            'slide-in-bck-left-out': 'slide-in-bck-left 1s ease-in-out 0s 1 normal forwards, fade-out-top 1s ease-in-out 5s 1 normal forwards',
+            "fade-in-bck": "fade-in-bck 0.7s cubic-bezier(0.390, 0.575, 0.565, 1.000)   both",
+            "typing": "typing 2s steps(20)  alternate, blink .7s "
         },
         keyframes: {
             "heartbeat": {
@@ -195,6 +199,43 @@ module.exports = {
             to: {
                 transform: "scaleX(1)",
                 opacity: "1"
+            }
+          },
+          "fade-out-top": {
+            "0%": {
+                transform: "translateY(0)",
+                opacity: "1"
+            },
+            to: {
+                transform: "translateY(-50px)",
+                opacity: "0"
+            }
+          },
+          "fade-in-bck": {
+            "0%": {
+                transform: "translateZ(80px)",
+                opacity: "0"
+            },
+            to: {
+                transform: "translateZ(0)",
+                opacity: "1"
+            }
+          },
+          "typing": {
+            "0%": {
+              width: "0%",
+              visibility: "hidden"
+            },
+            "100%": {
+              width: "100%"
+            }
+          },
+          "blink": {
+            "50%": {
+              borderColor: "transparent"
+            },
+            "100%": {
+              borderColor: "white"
             }
           }
         }
