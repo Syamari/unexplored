2d9eb3a67e0a1344b091c786556c13b221ce0f6e
Syamari <130308232+Syamari@users.noreply.github.com>
2024-07-02 02:15:26 +0900

    fix: listテストのbefore doを修正

--- spec/features/lists/list_spec.rb
+++ spec/features/lists/list_spec.rb
@@ -2,26 +2,25 @@ require 'rails_helper'
 
 RSpec.feature "リスト", type: :feature do
   before do
-    User.create!(user_name: "Test User", email: "test@example.com", password: "password123", password_confirmation: "password123")
+    # ユーザーの作成とログイン
+    user = User.create!(user_name: "Test User", email: "test@example.com", password: "password123", password_confirmation: "password123")
     visit login_path
     fill_in "email", with: "test@example.com"
     fill_in "password", with: "password123"
     click_button "ログイン"
     expect(page).to have_content "ログインしました"
-    @list_name = 'Test List'
 
+    # リストの作成とアーティストの追加
+    @list1 = List.create!(name: 'Test List', user_id: user.id)
+    @list2 = List.create!(name: 'Test List2', user_id: user.id)
+    ['Arctic Monkeys', 'Porcupine Tree', 'Portishead'].each do |artist_name|
+      artist = Artist.create!(name: artist_name)
+      ListArtist.create!(list_id: @list2.id, artist_id: artist.id)
+    end
+
+    # リスト作成のために必要な変数を定義
+    @list_name = 'Test List'
     @list_name2 = 'Test List2'
-    visit lists_path
-    click_button "新規リスト作成"
-    fill_in "list_name", with: @list_name2
-    click_button "登録"
-    click_link @list_name2
-    fill_in 'artist-name-input', with: 'Arctic Monkeys'
-    click_button 'このアーティストを追加'
-    fill_in 'artist-name-input', with: 'Porcupine Tree'
-    click_button 'このアーティストを追加'
-    fill_in 'artist-name-input', with: 'Portishead'
-    click_button 'このアーティストを追加'
   end
 
   context '入力情報正常系' do
@@ -38,9 +37,9 @@ RSpec.feature "リスト", type: :feature do
     it "リストにアーティストを追加する" do
       visit lists_path
       click_button "新規リスト作成"
-      fill_in "list_name", with: @list_name
+      fill_in "list_name", with: "Add Artist List"
       click_button "登録"
-      click_link @list_name
+      click_link "Add Artist List"
       fill_in 'artist-name-input', with: 'Arctic Monkeys'
       click_button 'このアーティストを追加'
       expect(page).to have_content('Arctic Monkeys')
