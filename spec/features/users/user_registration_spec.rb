0383bab6ef3b5077d529cd3431df236033ee011e
Syamari <130308232+Syamari@users.noreply.github.com>
2024-07-07 22:42:29 +0900

    fix: rspec user登録テストにエラーを追加

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -12,4 +12,24 @@ RSpec.feature "ユーザー登録", type: :feature do
       expect(page).to have_content "ユーザー登録 & ログインしました"
     end
   end
+
+  context '入力情報異常系' do
+    it 'メールアドレスが未入力の場合、エラーメッセージが表示されること' do
+      visit new_user_path
+      fill_in "user_email", with: ""
+      fill_in "user_password", with: "password123"
+      fill_in "user_password_confirmation", with: "password123"
+      click_button "登録"
+      expect(page).to have_content "ユーザー登録に失敗しました"
+    end
+
+    it 'パスワードが未入力の場合、エラーメッセージが表示されること' do
+      visit new_user_path
+      fill_in "user_email", with: "test@example.com"
+      fill_in "user_password", with: ""
+      fill_in "user_password_confirmation", with: ""
+      click_button "登録"
+      expect(page).to have_content "ユーザー登録に失敗しました"
+    end
+  end
 end
