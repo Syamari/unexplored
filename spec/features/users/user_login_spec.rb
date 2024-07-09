82e9a2aea40447966909569e58c2a0ecc97688a4
Syamari <130308232+Syamari@users.noreply.github.com>
2024-07-10 00:09:55 +0900

    fix: ログインテストにエラーの追加

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -12,4 +12,22 @@ RSpec.feature "ユーザーログイン", type: :feature do
       expect(page).to have_content "ログインしました"
     end
   end
+
+  context '入力情報異常系' do
+    it 'パスワードが未入力の場合、エラーメッセージが表示されること' do
+      visit login_path
+      fill_in "email", with: "test@example.com"
+      fill_in "password", with: ""
+      click_button "ログイン"
+      expect(page).to have_content "ログインに失敗しました"
+    end
+
+    it 'メールアドレスが未入力の場合、エラーメッセージが表示されること' do
+      visit login_path
+      fill_in "email", with: ""
+      fill_in "password", with: "password123"
+      click_button "ログイン"
+      expect(page).to have_content "ログインに失敗しました"
+    end
+  end
 end
